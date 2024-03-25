f0ca4b11fe1855ac64d1fe9cc0c06892e273d26a
Mohamed-Sofy <40831151+Mohamed-Sofy@users.noreply.github.com>
2024-03-25 23:45:29 +0200

    Update 1

--- app/src/main/java/com/fcai/post_project_using_mvvm/MainActivity.kt
+++ app/src/main/java/com/fcai/post_project_using_mvvm/MainActivity.kt
@@ -14,13 +14,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        // initalize view model
+        viewModel = ViewModelProvider(this).get(PostViewModel::class.java)
 
-
+        //Declare Recycler view
         post_recycler.layoutManager = LinearLayoutManager(this)
+
+        //Call Function from view model
+        viewModel.GetAllPosts()
+
         // Create and set adapter
-
-
-        viewModel.posts.observe(this,{ PostList->
+        viewModel.posts.observe(this,{ PostList ->
             val adapter = PostAdapter(PostList)
             post_recycler.adapter = adapter
         })
